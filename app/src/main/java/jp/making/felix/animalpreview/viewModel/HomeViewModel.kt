ec74379c13676b195632e00d1fa7cdb1429953fc
felix925 <45941545+felix925@users.noreply.github.com>
2020-05-17 13:18:10 +0900

    fix / models

--- app/src/main/java/jp/making/felix/animalpreview/viewModel/HomeViewModel.kt
+++ app/src/main/java/jp/making/felix/animalpreview/viewModel/HomeViewModel.kt
@@ -24,15 +24,6 @@ class HomeViewModel @Inject constructor() : ViewModel() {
     fun loadDogs() {
         // TODO: DataSourceをInjectできるようにしてそこから値を使う
         // TODO: いいね的な機能を実装する DBを整える必要性が出てくる Url,FabState
-        val imageList = listOf(
-            "https://images.dog.ceo/breeds/pomeranian/n02112018_354.jpg" to true,
-            "https://images.dog.ceo/breeds/appenzeller/n02107908_3155.jpg" to true,
-            "https://images.dog.ceo/breeds/setter-gordon/n02101006_2501.jpg" to true,
-            "https://images.dog.ceo/breeds/terrier-american/n02093428_1439.jpg" to true,
-            "https://images.dog.ceo/breeds/papillon/n02086910_7842.jpg" to true,
-            "https://images.dog.ceo/breeds/terrier-lakeland/n02095570_2872.jpg" to true
-        )
-//        _dogs.value = imageList
         viewModelScope.launch {
             val list = dogDataSource.getDogs(10)
             _dogs.value = buildList {
